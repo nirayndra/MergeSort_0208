45a7efd7c8cd3f18b0fc79899f36f3f9313f8a33
nirayndra <faadiajasmine@gmail.com>
2026-03-31 11:06:58 +0700

    Menambah validasi jumlah elemen array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -6,5 +6,18 @@ int n;
 
 void input()
 {
+while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
 
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimum panjang array yang dapat dimasukkan adalah 20:" << endl;
+        }
+    }
 }
