ad9c5e30398e04b8c2806db7f03d822574e88b1a
nirayndra <faadiajasmine@gmail.com>
2026-03-31 11:05:00 +0700

    Deklarasi array global dan variabel n

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,33 +1,5 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
+int arr[20], B[20];
 int n;
-
-void input()
-{
-    while (true)
-    { 
-        cout << "Masukkan panjang element array: ";
-        cin >> n;
-
-        if (n <= 20)
-        {
-            break;
-        }
-        else
-        {
-            cout << "\nMaksimum panjang array yang dapat dimasukkan adalah 20." << endl;
-        }
-    }
-    cout << endl;
-    cout << "================================================" << endl;
-    cout << "=============Masukkan Element Array=============" << endl;
-    cout << "================================================" << endl;
-    
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Data ke-" << i + 1 << ": ";
-        cin >> arr[i];
-    }
-}
