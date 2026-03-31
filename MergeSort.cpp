2692367f5dec2050862e2091cf2fca45eb717f93
nirayndra <faadiajasmine@gmail.com>
2026-03-31 11:11:18 +0700

    Menambah proses penggabungan

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -47,4 +47,37 @@ void mergeSort(int low, int high)
     mergeSort(low, mid); // step 3.a
     mergeSort(mid + 1, high); // step 3.b
 
+    //step 4
+    int i = low;
+    int j = mid +1;
+    int k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        j++;
+        k = k + 1;
+    }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
 }
